d7e48f54413152784760619ee34081e1cce33f9c
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-12-16 22:57:54 +0700

    delete default user img

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,6 @@ class UserSeeder extends Seeder
                 'name' => 'Admin',
                 'email' => 'admin@example.com',
                 'password' => bcrypt('admin'),
-                'profile_photo_path' => '/profile-photos/default.png',
             ]);
         }
         User::factory(10)->create();
