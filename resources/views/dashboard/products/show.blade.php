1348685d8eb3abffba091b9935e9f5ed852d2dd2
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2021-12-19 15:46:08 +0700

    fix title

--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -1,8 +1,8 @@
-<x-app-layout title="Dashboard | Categories">
+<x-app-layout title="Dashboard | Products">
 
     <div class="container grid px-6 mx-auto">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Categories
+            Product
         </h2>
 
         <div class="flex">
